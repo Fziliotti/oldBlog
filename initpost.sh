14e9abc1674ab0679b5630ebe0fc6ab9f2fb218d
Fabricio Ziliotti <ffz.ziliotti@gmail.com>
2018-08-19 18:54:09 -0300

    modificando scrip

--- initpost.sh
+++ initpost.sh
@@ -46,7 +46,7 @@ POSTPATH="${BINPATH}/_posts"
 DIST_FOLDER="$POSTPATH"
 
 # Set your blog URL
-BLOG_URL="https://willianjusten.com.br"
+BLOG_URL="https://fziliotti.github.io/"
 
 # Set your assets URL
 ASSETS_URL="assets/img/"
