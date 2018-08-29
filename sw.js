7819da0ace2b5ab3f5b3d1b9005e2ee09b35e7d6
Fabricio Ziliotti <ffz.ziliotti@gmail.com>
2018-08-29 11:52:27 -0300

    removendo o fetch

--- sw.js
+++ sw.js
@@ -36,14 +36,14 @@ this.addEventListener('activate', event => {
 });
 
 // EVENTO FETCH É ATIVADO TODA VEZ QUE UMA PAGINA É REQUISITADA
-this.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-      .catch(() => {
-        return caches.match('/offline.html');
-      })
-  )
-});
+// this.addEventListener("fetch", event => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(response => {
+//         return response || fetch(event.request);
+//       })
+//       .catch(() => {
+//         return caches.match('/offline.html');
+//       })
+//   )
+// });
